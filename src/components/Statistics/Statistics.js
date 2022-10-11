dbd90076bcbf4be1456181eb2d822f6946d48dde
Salmalisa <salmalisa7342@gmail.com>
2022-10-11 18:55:46 +0600

    total questions chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+  const loadedData = useLoaderData();
+  const quizData = loadedData.data;
   return (
-    <div>
-      
+    <div className='border'>
+       
+      <AreaChart className='mx-auto mt-16' width={550}
+          height={350}
+        data={quizData}>
+        <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+          <Area type="monotone" dataKey="total" stroke="#4A47D3" />
+        </AreaChart>
+       <h1 className='text-center'> Total Questions Chart</h1>
     </div>
   );
 };
